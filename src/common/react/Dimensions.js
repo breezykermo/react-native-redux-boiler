772e5ce65178fd2aafd34bcca6ce7d51d93ff29b
Lachlan Kermode <lachlankermode@live.com>
2016-07-06 17:02:34 -0700

    Add Dimensions util

---
+++ src/common/react/Dimensions.js
@@ -0,0 +1,7 @@
+import { Dimensions } from 'react-native'
+const WINDOW = Dimensions.get('window')
+
+export default {
+  width: WINDOW.width,
+  height: WINDOW.height,
+}
--- src/containers/App.styles.js
+++ src/containers/App.styles.js
@@ -1,8 +1,12 @@
+import { Dimensions } from '../common/react'
+
 export default {
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white',
+    width: Dimensions.width,
+    height: Dimensions.height,
   },
 }
