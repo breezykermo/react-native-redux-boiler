21454bcd466e1371fb7cba8bd8c45d99768f84a1
Lachlan Kermode <lachlankermode@live.com>
2016-07-16 10:53:51 -0700

    Delete test example

--- src/store/test/configureStore.spec.js
+++
@@ -1,8 +0,0 @@
-import configureStore from '../configureStore'
-import { expect } from 'chai'
-
-describe('configureStore', () => {
-  it('is a function', () => {
-    expect(typeof configureStore).to.equal('function')
-  })
-})
